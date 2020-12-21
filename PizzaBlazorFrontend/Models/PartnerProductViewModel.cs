f5251904f2bf3d9a0aec9d17c4e12b8e705108ce
DmitryGolubenkov <xxdimonxx15@yandex.ru>
2020-12-21 16:48:46 +0300

    Конечный вариант системы при сдаче

---
+++ PizzaBlazorFrontend/Models/PartnerProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace PizzaBlazorFrontend.Models
+{
+    public class PartnerProductViewModel
+    {
+        public int PartnerProductId { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int ProductPartnerPrice { get; set; }
+        public int ProductCount { get; set; }
+    }
+}
